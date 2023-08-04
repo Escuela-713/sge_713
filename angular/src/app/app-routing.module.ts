24eaf360a65cf22e60e88bfbc66d168f8810f6df
Ivana Rojas <mrojascorsico@gmail.com>
2023-08-04 09:41:26 -0300

    se agregó el componente datos-personales!

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,14 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CarrerasComponent } from './pages/carreras/carreras.component';
+import { DatosPersonalesComponent } from './pages/datos-personales/datos-personales.component';
 
 const routes: Routes = [
-  {path:"home" , component:HomeComponent
-  },
+  {path:"home" , component:HomeComponent},
   {path:"dashboard" , component:DashboardComponent,
-  children: [
-    {path:"carreras", component:CarrerasComponent }
-    ] },
+    children: [
+      {path:"carreras", component:CarrerasComponent },
+      {path:"datos-personales", component:DatosPersonalesComponent}
+      
+      ] },
   { path: '',   redirectTo: '/home', pathMatch: 'full'}
 ];
 
---
+++ angular/src/app/pages/datos-personales/datos-personales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-datos-personales',
+  templateUrl: './datos-personales.component.html',
+  styleUrls: ['./datos-personales.component.css']
+})
+export class DatosPersonalesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
