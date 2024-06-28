853151998b5beaa31ff66daa303bbc0c31351824
Ezequiel Daher <ezequieldaher06@gmail.com>
2024-06-28 11:24:42 -0300

    se modifico el registro

--- frontend/src/app/pages/gestion-usuarios/registro/registro.component.ts
+++ frontend/src/app/pages/gestion-usuarios/registro/registro.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
 })
