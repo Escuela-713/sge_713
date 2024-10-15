1f1b9a308281ad3fbc1b5118c2ac42c36136cc2d
unknown <simeonifilipo38@gmail.com>
2024-10-15 14:52:56 -0300

    se agrego formularios reactivos!

--- frontend/src/app/pages/gestion-usuarios/login/login.component.ts
+++ frontend/src/app/pages/gestion-usuarios/login/login.component.ts
@@ -1,13 +1,47 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  form!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group(
+      {
+        email: ['', [Validators.email, Validators.required]],
+        password: ['', [Validators.required]]
+      }
+    )
+
+  }
+
+  onEnviar(event: Event) {
+    console.log(this.form.value)
+
+    event.preventDefault;
+    if (this.form.valid) {
+      alert("Enviar al servidor...")
+    }
+    else {
+
+
+      this.form.markAllAsTouched();
+    }
+  }
+
+  get Password() {
+    return this.form.get("password");
+  }
+  get Email() {
+    return this.form.get("email");
+  }
+
 
 }
--- frontend/src/app/pages/gestion-usuarios/registro/registro.component.ts
+++ frontend/src/app/pages/gestion-usuarios/registro/registro.component.ts
@@ -1,13 +1,71 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,ReactiveFormsModule],
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.css'
+  
 })
 export class RegistroComponent {
+  form!:FormGroup;
+  constructor(private formBuilder: FormBuilder)
+  { 
+
+    this.form=this.formBuilder.group(
+      {
+        email:['',[Validators.email,Validators.required]],
+        password:['',[Validators.required]],
+        passwordconfirmation:['',[Validators.required]],
+        name:['',[Validators.required]],
+        surname:['',[Validators.required]],
+        cuil:['',[Validators.required]],
+        cuilconfirmation:['',[Validators.required]],
+      } 
+    )
+
+  }
+  
+  onEnviar(event: Event) {
+    console.log(this.form.value)
+
+    event.preventDefault;
+    if (this.form.valid) {
+      alert("Enviar al servidor...")
+    }
+    else {
+
+
+      this.form.markAllAsTouched();
+    }
+  }
+
+  get Password() {
+    return this.form.get("password");
+  }
+  get Passwordconfirmation() {
+    return this.form.get("passwordconfirmation");
+  }
+  get Email() {
+    return this.form.get("email");
+  }
+  get Name() {
+    return this.form.get["name"];
+  }
+  get Surname() {
+    return this.form.get("surname");
+  }
+  get Cuil() {
+    return this.form.get("cuil");
+  }
+  get Cuilconfirmation() {
+    return this.form.get("cuilconfirmation");
+  }
 
 }
+
