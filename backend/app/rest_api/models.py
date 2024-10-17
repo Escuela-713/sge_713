22013e36b63724b6eae81331abef4c9f0ef062b4
leonel-pala <linderman343@gmail.com>
2024-10-17 14:14:29 -0300

    correcion

--- backend/app/rest_api/models.py
+++ backend/app/rest_api/models.py
@@ -37,7 +37,8 @@ class Alumno(models.Model):
     n_libro_matriz = models.IntegerField(blank=True, null=True)
     fecha_egreso = models.DateField(db_column='fecha egreso', blank=True, null=True)  # Field renamed to remove unsuitable characters.
     cuil = models.CharField(max_length=15)
-    id_persona = models.ForeignKey(Persona, models.DO_NOTHING, db_column='id_persona')
+
+    id_persona = models.ForeignKey(Persona, models.DO_NOTHING, db_column='id_persona', null=True)
 
 
     class Meta:
