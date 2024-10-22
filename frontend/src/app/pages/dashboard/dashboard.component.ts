0b8f0a034167ddeebc2638a63079973920fb8148
Gianlucca Basilio Quinteros Oszust <gianlubasilio@gmail.com>
2024-10-22 14:02:22 -0300

    Actualización general de los estilos

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
+import { HeaderComponent } from 'src/app/shared/header/header.component';
 import { NavComponent } from 'src/app/shared/nav/nav.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NavComponent,RouterOutlet,FooterComponent],
+  imports: [NavComponent,RouterOutlet,FooterComponent, HeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
