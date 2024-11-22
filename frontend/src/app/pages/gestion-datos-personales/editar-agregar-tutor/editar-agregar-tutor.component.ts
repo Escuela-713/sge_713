9ab3663ed8c9e10c7ec5295d40cd5c01e3cbb658
Manuel Kadomoto <manukado18@gmail.com>
2024-11-22 08:08:13 -0300

    Se agregaron las notificaciones de error

--- frontend/src/app/pages/gestion-datos-personales/editar-agregar-tutor/editar-agregar-tutor.component.ts
+++ frontend/src/app/pages/gestion-datos-personales/editar-agregar-tutor/editar-agregar-tutor.component.ts
@@ -5,7 +5,7 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 @Component({
   selector: 'app-editar-agregar-tutor',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './editar-agregar-tutor.component.html',
   styleUrl: './editar-agregar-tutor.component.css'
 })
@@ -13,6 +13,12 @@ export class EditarAgregarTutorComponent {
   datosTutorForm: FormGroup;
   constructor( private formBuilder: FormBuilder, private router: Router) {
     this.datosTutorForm = this.formBuilder.group({
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      dni: ['', Validators.required,],
+      cuil: ['', Validators.required],
+      genero: ['', Validators.required],
+      trabajo: ['', Validators.required],
       fechaNacimiento: ['', Validators.required],
       provinciaNacimiento: ['', Validators.required],
       localidadNacimiento: ['', Validators.required,],
@@ -24,7 +30,136 @@ export class EditarAgregarTutorComponent {
       nroPiso: [''],
       nroDpto: [''],
       telefono: ['', Validators.required],
-      mailAlumno: ['', [Validators.required, Validators.email]],
+      mailTutor: ['', [Validators.required, Validators.email]],
     });
   }
+  get nombre() {
+    return this.datosTutorForm.controls['nombre'];
+  }
+  get nombreErrors() {
+    const errors = this.nombre.errors;
+    return errors ? (errors['required'] ? 'El nombre es obligatorio.' : null) : null;
+  }
+  
+  get apellido() {
+    return this.datosTutorForm.controls['apellido'];
+  }
+  get apellidoErrors() {
+    const errors = this.apellido.errors;
+    return errors ? (errors['required'] ? 'El apellido es obligatorio.' : null) : null;
+  }
+  
+  get dni() {
+    return this.datosTutorForm.controls['dni'];
+  }
+  get dniErrors() {
+    const errors = this.dni.errors;
+    return errors ? (errors['required'] ? 'El DNI es obligatorio.' : null) : null;
+  }
+  
+  get cuil() {
+    return this.datosTutorForm.controls['cuil'];
+  }
+  get cuilErrors() {
+    const errors = this.cuil.errors;
+    return errors ? (errors['required'] ? 'El CUIL es obligatorio.' : null) : null;
+  }
+  
+  get genero() {
+    return this.datosTutorForm.controls['genero'];
+  }
+  get generoErrors() {
+    const errors = this.genero.errors;
+    return errors ? (errors['required'] ? 'El género es obligatorio.' : null) : null;
+  }
+  
+  get trabajo() {
+    return this.datosTutorForm.controls['trabajo'];
+  }
+  get trabajoErrors() {
+    const errors = this.trabajo.errors;
+    return errors ? (errors['required'] ? 'El trabajo es obligatorio.' : null) : null;
+  }
+  
+  get fechaNacimiento() {
+    return this.datosTutorForm.controls['fechaNacimiento'];
+  }
+  get fechaNacimientoErrors() {
+    const errors = this.fechaNacimiento.errors;
+    return errors ? (errors['required'] ? 'La fecha de nacimiento es obligatoria.' : null) : null;
+  }
+  
+  get provinciaNacimiento() {
+    return this.datosTutorForm.controls['provinciaNacimiento'];
+  }
+  get provinciaNacimientoErrors() {
+    const errors = this.provinciaNacimiento.errors;
+    return errors ? (errors['required'] ? 'La provincia de nacimiento es obligatoria.' : null) : null;
+  }
+  
+  get localidadNacimiento() {
+    return this.datosTutorForm.controls['localidadNacimiento'];
+  }
+  get localidadNacimientoErrors() {
+    const errors = this.localidadNacimiento.errors;
+    return errors ? (errors['required'] ? 'La localidad de nacimiento es obligatoria.' : null) : null;
+  }
+  
+  get paisDomicilio() {
+    return this.datosTutorForm.controls['paisDomicilio'];
+  }
+  get paisDomicilioErrors() {
+    const errors = this.paisDomicilio.errors;
+    return errors ? (errors['required'] ? 'El país del domicilio es obligatorio.' : null) : null;
+  }
+  
+  get provinciaDomicilio() {
+    return this.datosTutorForm.controls['provinciaDomicilio'];
+  }
+  get provinciaDomicilioErrors() {
+    const errors = this.provinciaDomicilio.errors;
+    return errors ? (errors['required'] ? 'La provincia del domicilio es obligatoria.' : null) : null;
+  }
+  
+  get localidadDomicilio() {
+    return this.datosTutorForm.controls['localidadDomicilio'];
+  }
+  get localidadDomicilioErrors() {
+    const errors = this.localidadDomicilio.errors;
+    return errors ? (errors['required'] ? 'La localidad del domicilio es obligatoria.' : null) : null;
+  }
+  
+  get barrio() {
+    return this.datosTutorForm.controls['barrio'];
+  }
+  get barrioErrors() {
+    const errors = this.barrio.errors;
+    return errors ? (errors['required'] ? 'El barrio es obligatorio.' : null) : null;
+  }
+  
+  get calle() {
+    return this.datosTutorForm.controls['calle'];
+  }
+  get calleErrors() {
+    const errors = this.calle.errors;
+    return errors ? (errors['required'] ? 'La calle es obligatoria.' : null) : null;
+  }
+  
+  get telefono() {
+    return this.datosTutorForm.controls['telefono'];
+  }
+  get telefonoErrors() {
+    const errors = this.telefono.errors;
+    return errors ? (errors['required'] ? 'El teléfono es obligatorio.' : null) : null;
+  }
+  
+  get mailTutor() {
+    return this.datosTutorForm.controls['mailTutor'];
+  }
+  get mailTutorErrors() {
+    const errors = this.mailTutor.errors;
+    return errors ? 
+      (errors['required'] ? 'El email es obligatorio.' : errors['email'] ? 'El email ingresado es inválido.' : null) : null;
+  }
+  
 }
--- frontend/src/app/pages/gestion-datos-personales/filtro-datos-personales/filtro-datos-personales.component.ts
+++ frontend/src/app/pages/gestion-datos-personales/filtro-datos-personales/filtro-datos-personales.component.ts
@@ -20,4 +20,43 @@ export class FiltroDatosPersonalesComponent {
       year: ['', Validators.required,]
     });
   }
+  get curso() {
+    return this.filter.controls['curso'];
+  }
+   
+  get division() {
+    return this.filter.controls['division'];
+  }
+
+  get modalidad() {
+    return this.filter.controls['modalidad'];
+  }
+   
+  get year() {
+    return this.filter.controls['year'];
+  }
+
+  get isFormValid() {
+    return this.filter.valid;
+  }
+
+  get cursoErrors() {
+    const errors = this.curso.errors;
+    return errors ? (errors['required'] ? 'El curso es obligatorio.' : null) : null;
+  }
+
+  get divisionErrors() {
+    const errors = this.division?.errors;
+    return errors ? (errors['required'] ? 'La división es obligatoria' : null) : null;
+  }
+
+  get modalidadErrors() {
+    const errors = this.modalidad.errors;
+    return errors ? (errors['required'] ? 'La modalidad es obligatoria.' : null) : null;
+  }
+
+  get yearErrors() {
+    const errors = this.year?.errors;
+    return errors ? (errors['required'] ? 'El año es obligatorio' : null) : null;
+  }
 }
