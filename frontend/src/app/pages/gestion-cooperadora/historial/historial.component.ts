422d05ec9a28efa256d7154a282f9dc5b0e16be7
Lautaro <lautarodaniel1401@gmail.com>
2025-09-03 18:40:04 +0000

    cree el componente historial

---
+++ frontend/src/app/pages/gestion-cooperadora/historial/historial.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial',
+  imports: [],
+  templateUrl: './historial.component.html',
+  styleUrl: './historial.component.css'
+})
+export class HistorialComponent {
+
+}
