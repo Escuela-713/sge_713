d99ac2b3d2c17c8e7345f02926abee8ceaa05ab2
Walter Carrasco <95136445+someone1a@users.noreply.github.com>
2025-09-03 19:18:27 +0000

    Correcciones en el dashboard

--- frontend/src/app/pages/gestion-home/gestion-home.component.ts
+++ frontend/src/app/pages/gestion-home/gestion-home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
@@ -8,9 +8,18 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   templateUrl: './gestion-home.component.html',
   styleUrls: ['./gestion-home.component.css']
 })
-export class GestionHomeComponent {
-ngOnInit(): void{
-  document.title = `Home Dashboard - SGE 713  `
-}
+export class GestionHomeComponent implements AfterViewInit {
 
+  @ViewChild('menuToggle') menuToggle!: ElementRef<HTMLButtonElement>;
+  @ViewChild('sidebar') sidebar!: ElementRef<HTMLElement>;
+
+  ngAfterViewInit(): void {
+    document.title = `Home Dashboard - SGE 713`;
+
+    if (this.menuToggle && this.sidebar) {
+      this.menuToggle.nativeElement.addEventListener('click', () => {
+        this.sidebar.nativeElement.classList.toggle('active');
+      });
+    }
+  }
 }
