6427fb4611e2bd613f9972996fa3c741f510ba34
Axel Gonzalez <axelgonzalezyf28@gmail.com>
2025-09-16 14:24:24 -0300

    se agregaron las validaciones

---
+++ frontend/src/app/pages/gestion-cooperadora/registro-ingresos/registro-ingresos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-ingresos',
+  imports: [],
+  templateUrl: './registro-ingresos.component.html',
+  styleUrl: './registro-ingresos.component.css'
+})
+export class RegistroIngresosComponent {
+
+}
