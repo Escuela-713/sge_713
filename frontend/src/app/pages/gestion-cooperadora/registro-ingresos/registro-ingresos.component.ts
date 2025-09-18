9f2733ae62d7dc596064cd816f4de9e22270964f
Axel Gonzalez <axelgonzalezyf28@gmail.com>
2025-09-18 15:30:31 -0300

    cambie el ts de registro

--- frontend/src/app/pages/gestion-cooperadora/registro-ingresos/registro-ingresos.component.ts
+++ frontend/src/app/pages/gestion-cooperadora/registro-ingresos/registro-ingresos.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
 @Component({
   selector: 'app-registro-ingresos',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './registro-ingresos.component.html',
   styleUrl: './registro-ingresos.component.css'
 })
 export class RegistroIngresosComponent {
+form!:FormGroup;
+constructor(private formBuilder:FormBuilder)
+{
+  this.form = this.formBuilder.group(
+    {
+      monto:[''],
+      fecha:[''],
+      motivo:['']
 
+    }
+  );
 }
