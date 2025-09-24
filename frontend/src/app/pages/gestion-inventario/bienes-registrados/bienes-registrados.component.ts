ed55e0bd754e41cb7152b4417acbdc62ba678902
bruno <almoancidbrunosantiago@gmail.com>
2025-09-24 17:17:18 -0300

    inventario y links del formulario

--- frontend/src/app/pages/gestion-inventario/bienes-registrados/bienes-registrados.component.ts
+++ frontend/src/app/pages/gestion-inventario/bienes-registrados/bienes-registrados.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-bienes-registrados',
   templateUrl: './bienes-registrados.component.html',
-  styleUrls: ['./bienes-registrados.component.css']
+  styleUrls: ['./bienes-registrados.component.css'],
+  imports: [RouterLink]
 })
 export class BienesRegistradosComponent {
 
