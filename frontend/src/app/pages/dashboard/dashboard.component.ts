ccf677552bd73370e01437d756992196317d7335
cande-2311 <c4nde.polanco@gmail.com>
2025-10-23 19:44:57 +0000

    cambios en el html del acta volante

--- frontend/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from 'src/app/shared/footer/footer.component';
 import { HeaderComponent } from 'src/app/shared/header/header.component';
 import { NavComponent } from 'src/app/shared/nav/nav.component';
+import { }
 
 @Component({
   selector: 'app-dashboard',
