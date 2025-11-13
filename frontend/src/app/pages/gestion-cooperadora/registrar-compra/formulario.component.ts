9d1df5b25b52714eab322ff8bc0ed4ea28122f89
LautaroETP <nismannatalioalberto@gmail.com>
2025-11-13 19:20:21 +0000

    casi terminadas las validaciones

--- frontend/src/app/pages/gestion-cooperadora/registrar-compra/formulario.component.ts
+++ frontend/src/app/pages/gestion-cooperadora/registrar-compra/formulario.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
 export class FormularioComponent {
+  
+  FormularioComponentForm: FormGroup;
 
+  constructor(private formbuilder: FormBuilder) {
+    this.FormularioComponentForm = this.formbuilder.group({
+
+    
+
+    });
+  }
 }
