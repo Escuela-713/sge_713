698810c49bc1c5da2fe88790041532f5108abb30
Yaco <yacoledesma15@hotmail.com>
2025-11-14 10:19:49 -0300

    Se cambio el GET

--- backend/gestion_mesa_examenes/serializers.py
+++ backend/gestion_mesa_examenes/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import MesasExamenes
+from .models import Profesores
+from .models import Modalidades
+from gestion_datos_personales.models import Alumno
+
+
+
+class ModalidadesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Modalidades 
+        fields = '__all__'
+
+class ProfesoresSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profesores
+        fields = '__all__'
 
 class MesasExamenesSerializer(serializers.ModelSerializer):
+    profesor = ProfesoresSerializer()
+    modalidad = ModalidadesSerializer()
+   
     class Meta:
         model = MesasExamenes
         fields = '__all__'
-
