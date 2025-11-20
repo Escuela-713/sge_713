f343de7253e0fa1ee41a7353b3ae657a5eab1fdf
Yaco <yacoledesma15@hotmail.com>
2025-11-20 16:49:05 -0300

    segundo intento de POST

--- backend/gestion_mesa_examenes/serializers.py
+++ backend/gestion_mesa_examenes/serializers.py
@@ -6,26 +6,26 @@ from gestion_datos_personales.models import Alumno
 
 
 
-class ModalidadesSerializer(serializers.ModelSerializer):
-     class Meta:
+from rest_framework import serializers
+from .models import MesasExamenes
+
+class MesasExamenesSerializer(serializers.ModelSerializer):
+    class Meta:
         model = MesasExamenes
         fields = '__all__'
         extra_kwargs = {
-            'id_mesas_examenes': {'required': True},
             'id_profesores': {'required': False, 'allow_null': True},
             'id_modalidades': {'required': False, 'allow_null': True},
             'id_materias': {'required': False, 'allow_null': True},
             'id_alumno': {'required': False, 'allow_null': True},
         }
-        
+
 class ProfesoresSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profesores
         fields = '__all__'
 
-class MesasExamenesSerializer(serializers.ModelSerializer):
-    id_profesores = ProfesoresSerializer()
-    id_modalidades = ModalidadesSerializer()
+
    
     class Meta:
         model = MesasExamenes
