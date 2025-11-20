3d52e61d711c8a440560c0b902a1f7e6a82e1405
Yaco <yacoledesma15@hotmail.com>
2025-11-20 16:23:43 -0300

    primer intento de POST

--- backend/gestion_mesa_examenes/serializers.py
+++ backend/gestion_mesa_examenes/serializers.py
@@ -7,10 +7,17 @@ from gestion_datos_personales.models import Alumno
 
 
 class ModalidadesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Modalidades 
+     class Meta:
+        model = MesasExamenes
         fields = '__all__'
-
+        extra_kwargs = {
+            'id_mesas_examenes': {'required': True},
+            'id_profesores': {'required': False, 'allow_null': True},
+            'id_modalidades': {'required': False, 'allow_null': True},
+            'id_materias': {'required': False, 'allow_null': True},
+            'id_alumno': {'required': False, 'allow_null': True},
+        }
+        
 class ProfesoresSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profesores
