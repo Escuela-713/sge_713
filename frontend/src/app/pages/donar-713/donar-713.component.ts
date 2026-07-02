001b57e815d55de763a53b9ce17031fc7a04906f
manutosetti <mmanuelltt@gmail.com>
2026-07-02 08:12:26 -0300

    arreglos rutas

--- frontend/src/app/pages/donar-713/donar-713.component.ts
+++ frontend/src/app/pages/donar-713/donar-713.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+ import { CarrouselComponent } from "../donar-713/componentes/carrousel/carrousel.component";
+ import {  } from "../donar-713/componentes/eventos/eventos.component";
 @Component({
   selector: 'app-donar-713',
-  imports: [],
+  imports: [RouterLink,CarrouselComponent,RouterOutlet],
   templateUrl: './donar-713.component.html',
   styleUrl: './donar-713.component.css',
 })
