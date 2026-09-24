320abbcd59233f85e26ac33ee4887b7784ef244d
Angeles Pietra <angeles10pietra@gmail.com>
2026-09-24 15:23:38 -0300

    exporte publication, y agregue el postNovedad

--- frontend/src/app/services/novedades.service.ts
+++ frontend/src/app/services/novedades.service.ts
@@ -2,10 +2,24 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 
+export interface Publication {
+  id : string
+	title : string
+	content : string
+	image : string
+	is_published : boolean 
+	upload_date : string
+	update_date : string
+}
+
 @Injectable({
   providedIn: 'root'
 })
-
 export class NovedadesService {
   private apiUrl = 'http://127.0.0.1:8000/home/';
+  constructor(private http: HttpClient) {}
+
+  postNovedad(newNovedad: any): Observable<any> {
+    return this.http.post(this.apiUrl, newNovedad);
+  } 
 }
