a9212169a6d4503ce535b765d96bb13d406690b1
ElexemSDdR <valendomta@gmail.com>
2026-09-23 23:13:56 -0300

    Eliminé los componentes de las mesas de examen que no usabamos, el registro, el inicio y la inscripción y actualicé las rutas del proyecto

--- frontend/src/app/pages/gestion-examenes-finales/mesas-inicio/mesas-inicio.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-mesas-inicio',
-  templateUrl: './mesas-inicio.html',
-  styleUrl: './mesas-inicio.css',
-})
-export class MesasInicioComponent {}
