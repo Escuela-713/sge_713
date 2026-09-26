76f8211a8c273f43cbfc7843d334598fa445086f
Fran Pacheco <franpacheco.cpp@gmail.com>
2026-09-25 22:22:55 -0300

    fallos de cors

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -49,9 +49,9 @@ else:
 # Application definition
 
 INSTALLED_APPS = [
-    "corsheaders",
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -68,14 +68,19 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
 ]
 
 SIMPLE_JWT = {
@@ -184,3 +189,4 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CORS_ALLOW_CREDENTIALS = True
+
